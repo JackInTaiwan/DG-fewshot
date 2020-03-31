af1da5d79f1563b04547967edf05b0e759a84b79
jackintaiwan <jackconvolution@gmail.com>
2020-03-31 13:05:19 +0800

    refactor: move shared functions into the class TrainerBase

--- trainer/proto_cross_domain_sampling.py
+++ trainer/proto_cross_domain_sampling.py
@@ -5,12 +5,13 @@ import logging
 import shutil
 import torch
 import numpy as np
+import util.warmup as warmup
 
 from tqdm import tqdm
 from torch import nn, optim
 from tensorboardX import SummaryWriter
-import util.warmup as warmup
 
+from .trainer_base import TrainerBase
 from model import PrototypicalNet
 from data_loader import CrossDomainSamplingDataLoader as SamplingDataLoader
 from data_loader import CrossDomainSamplingEvalDataLoader
@@ -21,7 +22,7 @@ logger = logging.getLogger(__name__)
 
 
 
-class ProtoCrossDomainSamplingTrainer:
+class ProtoCrossDomainSamplingTrainer(TrainerBase):
     def __init__(self, config, mode, use_cpu):
         super().__init__()
 
@@ -97,7 +98,10 @@ class ProtoCrossDomainSamplingTrainer:
         elif self.mode_config["optimizer"] == "Adam":
             self.optim = optim.Adam(self.model.parameters(), lr=lr)
         
-        self.warmup_scheduler = warmup.LinearWarmup(self.optim, warmup_period=self.mode_config["warmup_period"])
+        self.warmup_scheduler = warmup.LinearWarmup(
+            self.optim,
+            warmup_period=self.mode_config["warmup_period"],
+        )
 
 
     def build_loss(self):
@@ -207,111 +211,6 @@ class ProtoCrossDomainSamplingTrainer:
 
         # save trained model at the end
         self.save_model()
-
-
-    @staticmethod
-    def calculate_acc(distances, labels):
-        pred_labels = torch.argmin(distances, dim=1)
-        acc = torch.mean((pred_labels == labels).to(dtype=torch.float32)).unsqueeze(0)
-
-        return acc
-
-
-    def validation_run(self):
-        logger.info("| Run validation ...")
-
-        self.model.eval()
-
-        total_acc_list = torch.tensor([])
-        
-        while self.val_data_loader.update_episode():
-            support_data = self.val_data_loader.get_support_images()
-            support_data = support_data.to(self.device)
-
-            self.model.keep_support_features(support_data)
-
-            total_distances, total_labels = torch.tensor([]), torch.tensor([])
-            for (query_images, query_labels) in self.val_data_loader:
-                query_images = query_images.to(self.device)
-                distances = self.model.inference(query_images)
-                distances = distances.detach().cpu()
-                total_distances = torch.cat([total_distances, distances])
-                total_labels = torch.cat([total_labels, query_labels])
-            
-            self.model.clean_support_features()
-
-            acc = self.calculate_acc(total_distances, total_labels)
-            total_acc_list = torch.cat([total_acc_list, acc])
-
-        self.val_data_loader.reset_episode()
-
-        avg_acc = total_acc_list.mean().item()
-        
-        logger.info("| Average Acc: {:.5f}".format(avg_acc))
-        
-        self.writer.add_scalar("val_acc", avg_acc, self.global_step)
-        self.writer.flush()
-
-        self.model.train()
-        
-        logger.info("| Finish validation.")
-
-
-    def save_model(self):
-        save_dir = self.config["checkpoint.save_dir"]
-        if not os.path.exists(save_dir):
-            os.makedirs(save_dir)
-
-        save_path = os.path.join(save_dir, '{}.checkpoint.pkl'.format(self.__class__.__name__))
-
-        # Dump the state_dict of model in cpu mode
-        self.model.cpu()
-        model_state_dict = self.model.state_dict()
-        optim_state_dict = self.optim.state_dict()
-
-        # NOTE: do remember to move model back to right device because we just called self.model.cpu() above
-        self.model.to(self.device)
-        
-        # Save checkpoint
-        torch.save({
-            "model_state_dict": model_state_dict,
-            "optim_state_dict": optim_state_dict,
-            "global_step": self.global_step,
-            },
-            save_path
-        )
-
-        logger.info('| Checkpoint is saved successfully in \'{}\''.format(save_path))
-
-
-    def load_checkpoint(self):
-        if self.mode in ["train", "resume"]:
-            logger.info("| Load checkpoint from {} ...".format(self.config["checkpoint.load_path"]))
-
-            checkpoint = torch.load(self.config["checkpoint.load_path"])
-
-            # load model state_dict
-            
-            self.model.load_state_dict(checkpoint["model_state_dict"])
-
-            # load optim state_dict
-            self.optim.load_state_dict(checkpoint["optim_state_dict"])
-
-            # load global step
-            self.global_step = checkpoint["global_step"]
-
-            logger.info('| Model is loaded successfully from \'{}\''.format(self.config["checkpoint.load_path"]))
-        
-        elif self.mode == "eval":
-            logger.info("| Load checkpoint from {} ...".format(self.config["checkpoint.load_path"]))
-
-            checkpoint = torch.load(self.config["checkpoint.load_path"])
-            loaded_model_state_dict = checkpoint["model_state_dict"]
-
-            state_dict = self.model.state_dict()
-            
-            # load global step
-            self.global_step = checkpoint["global_step"]
 
 
 
