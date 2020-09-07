18cf49629de4457615e8f4a3fce885478af1ceec
jackintaiwan <jackconvolution@gmail.com>
2020-09-07 14:35:35 +0800

    refactor: clean codes

--- trainer/__init__.py
+++ trainer/__init__.py
@@ -15,11 +15,6 @@ from . import (
 TRAINERS = {
     "RelCrossDomainSampling": rel_cross_domain_sampling.RelCrossDomainSamplingTrainer,
     "RelIdenticalDomainSampling": rel_identical_domain_sampling.RelIdenticalDomainSamplingTrainer,
-    "MixCrossDomainSampling": mix_cross_domain_sampling.MixCrossDomainSamplingTrainer,
-    "IdenticalCrossDomainSampling": identical_cross_domain_sampling.IdenticalCrossDomainSamplingTrainer,
     "ProtoCrossDomainSampling": proto_cross_domain_sampling.ProtoCrossDomainSamplingTrainer,
-    "ProtoIdenticalDomainSampling": proto_identical_domain_sampling.ProtoIdenticalDomainSamplingTrainer,
-    "ProtoMixDomainSampling": proto_mix_domain_sampling.ProtoMixDomainSamplingTrainer,
-    "ProtoPseudoDomainSampling": proto_pseudo_domain_sampling.ProtoPseudoDomainSamplingTrainer,
-    "ProtoCrossDomainBDBSampling": proto_drop_cross_domain_sampling.ProtoCrossDomainSamplingTrainer,
+    "ProtoIdenticalDomainSampling": proto_identical_domain_sampling.ProtoIdenticalDomainSamplingTrainer
 }
